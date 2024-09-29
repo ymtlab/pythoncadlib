80be336c3386bed0111bb52714cbc8592c04bacf
ymtlab <ymt.lab8@gmail.com>
2024-09-29 11:41:12 +0900

    main

--- test_pyside6_treeview.py
+++ test_pyside6_treeview.py
@@ -1,57 +1,3 @@
-# 使い方
-
-## ツリーをCSV出力するサンプル
-
-```
-import csv
-import pycadsx
-
-def main():
-    cad = pycadsx.PyCadSx()
-    cad.get_inf_sys()
-    cad.active_model.get_tree()
-
-    output_list = [ ['Level', 'Name', 'Comment'] ]
-
-    stack: list[tuple[int, pycadsx.Part]] = [(0, cad.active_model.top_part)]
-    while stack:
-        level, part = stack.pop()
-        output_data = [level, part.name, part.comment]
-        output_list.append(output_data)
-        for child in part.children:
-            stack.append((level + 1, child))
-    
-    with open('tree.csv', 'w', newline='', encoding='utf-8') as f:
-        writer = csv.writer(f)
-        writer.writerows(output_list)
-
-if __name__ == '__main__':
-    main()
-```
-
-## パーツを追加するサンプル
-
-```
-import pycadsx
-
-def main():
-    cad = pycadsx.PyCadSx()
-    cad.get_inf_sys()
-    cad.active_model.get_tree()
-
-    for i in range(3):
-        child = cad.active_model.top_part.create_child(f'name{i:03d}', f'comment{i:03d}')
-        
-        for j in range(3):
-            child.create_child(f'name{i:03d}-{j:03d}', f'comment{i:03d}-{j:03d}')
-
-if __name__ == '__main__':
-    main()
-```
-
-## PySide6のQTreeViewにツリーを表示するサンプル
-
-```
 from PySide6 import QtWidgets, QtGui
 import pycadsx
 
@@ -107,4 +53,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-```
